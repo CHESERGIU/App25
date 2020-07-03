4537eb6f9d45d1db9155c0a5081c960ef735afe3
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:51:45 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace App25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int minim = 0;
+            int count = 0;
+            do
+            {
+                input = Console.ReadLine();
+                if (input == "0")
+                {
+                    break;
+                }
+                else
+                {
+                    count++;
+                    int value = int.Parse(input);
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (value < minim)
+                        {
+                            minim = value;
+                        }
+                        count--;
+                    }
+                }
+            } while (!(input == "0"));
+            Console.WriteLine(minim);
+        }
+    }
+}
